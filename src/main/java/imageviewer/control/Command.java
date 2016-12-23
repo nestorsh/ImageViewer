bf4f4a807b737d1369c273058eec851567bcb381
Néstor <Nstor@Nstor>
2016-12-23 23:49:25 +0000

    ImageViewer: versión 1

---
+++ src/main/java/imageviewer/control/Command.java
@@ -0,0 +1,6 @@
+package imageviewer.control;
+
+public interface Command {
+    String name();
+    void execute();
+}
---
+++ src/main/java/imageviewer/ui/ImageDisplay.java
@@ -0,0 +1,10 @@
+package imageviewer.ui;
+
+import imageviewer.model.Image;
+
+public interface ImageDisplay {
+    
+    Image currentImage();
+    void display(Image image);
+    
+}
